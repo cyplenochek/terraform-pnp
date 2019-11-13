a01a2d8dc019f683f3c1677040f3c10c1b0a722b
U6106829 <u6106829@u6106829-tpm-a.ten.thomsonreuters.com>
2019-11-13 12:49:51 -0500

    renamed Build to Test stage

--- remote_backend.tf
+++ remote_backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~>0.10"
   backend "s3" {
     encrypt = "true"
-    bucket  = "tarraform-backend-nakyusha"
+    bucket  = "tarraform-backend-nadyusha"
     key     = "ourdatastore/terraform.tfstate"
     region  = "us-east-1"
   }
