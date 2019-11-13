6baee70a66aaf4b6a2e1b7a91d74ffa262782275
U6106829 <u6106829@u6106829-tpm-a.ten.thomsonreuters.com>
2019-11-13 12:59:41 -0500

    renamed s3 bucket

--- remote_backend.tf
+++ remote_backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~>0.10"
   backend "s3" {
     encrypt = "true"
-    bucket  = "tarraform-backend-nadyusha"
+    bucket  = "tarraform-backend-nakyusha"
     key     = "ourdatastore/terraform.tfstate"
     region  = "us-east-1"
   }
